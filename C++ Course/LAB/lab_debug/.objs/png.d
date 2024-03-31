dc2cdc13b0cc35dc1ccc81aa1ffd55393c42099a
Don Hämäläinen <118666926+donhamalainen@users.noreply.github.com>
2024-03-31 18:53:14 +0300

    update

---
+++ C++ Course/LAB/lab_debug/.objs/png.d
@@ -0,0 +1,14 @@
+.objs/png.o: png.cpp png.h \
+  /opt/homebrew/opt/libpng/include/libpng16/png.h \
+  /opt/homebrew/opt/libpng/include/libpng16/pnglibconf.h \
+  /opt/homebrew/opt/libpng/include/libpng16/pngconf.h rgbapixel.h
+
+png.h:
+
+/opt/homebrew/opt/libpng/include/libpng16/png.h:
+
+/opt/homebrew/opt/libpng/include/libpng16/pnglibconf.h:
+
+/opt/homebrew/opt/libpng/include/libpng16/pngconf.h:
+
+rgbapixel.h:
---
+++ C++ Course/LAB/lab_debug/.objs/sketchify.d
@@ -0,0 +1,14 @@
+.objs/sketchify.o: sketchify.cpp png.h \
+  /opt/homebrew/opt/libpng/include/libpng16/png.h \
+  /opt/homebrew/opt/libpng/include/libpng16/pnglibconf.h \
+  /opt/homebrew/opt/libpng/include/libpng16/pngconf.h rgbapixel.h
+
+png.h:
+
+/opt/homebrew/opt/libpng/include/libpng16/png.h:
+
+/opt/homebrew/opt/libpng/include/libpng16/pnglibconf.h:
+
+/opt/homebrew/opt/libpng/include/libpng16/pngconf.h:
+
+rgbapixel.h:
